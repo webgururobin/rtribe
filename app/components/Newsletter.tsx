fb84288ef2cddf49156fa3525b48ac619703a1c5
Robin Solanki <robinsolanki@me.com>
2025-01-31 20:59:20 +0530

    tweak: add loading

--- app/components/Newsletter.tsx
+++ app/components/Newsletter.tsx
@@ -9,22 +9,26 @@ import { toast } from 'sonner'
 const Newsletter = () => {
     const [email, setEmail] = useState<string>('')
     const [state, setState] = useState<{ success: boolean } | null>(null)
+    const [loading, setLoading] = useState<boolean>(false)
 
     const formAction = async (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
         const formData = new FormData(event.currentTarget)
 
         try {
+            setLoading(true)
             const result = await handleRegister(state, formData)
             setState(result)
 
             if (result?.success) {
                 toast("You'll be notified soon!")
                 setEmail('')
+                setLoading(false)
             }
         } catch (error) {
             console.error('Registration failed:', error)
             toast.error("Error registering. Please try again.")
+            setLoading(false)
         }
     }
 
@@ -43,6 +47,7 @@ const Newsletter = () => {
                 type='submit'
                 variant='ghost'
                 className='border border-orange font-nippo font-bold text-2xl text-orange uppercase bg-none rounded-none py-6 w-fit'
+                disabled={loading}
             >
                 Get Notified
             </Button>
